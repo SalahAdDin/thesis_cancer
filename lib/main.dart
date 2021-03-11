5ea4bc9bf8dc95c13f6263e976b718c238d3e01e
Luis Alaguna <alagunasalahaddin@live.com>
2021-03-11 11:09:09 +0300

    :tada: implementing simple sign up workflow.

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,29 @@
+import 'package:amplify_auth_cognito/amplify_auth_cognito.dart';
+import 'package:amplify_flutter/amplify.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_login/flutter_login.dart';
+import 'package:thesis_cancer/amplifyconfiguration.dart';
 
-void main() {
-  runApp(MyApp());
+enum displayState {
+  SIGNED_IN,
+  SHOW_CONFIRM,
+  SHOW_CONFIRM_RESET,
+  SHOW_CONFIRM_SIGN_IN,
+  SHOW_SIGN_IN,
+  SHOW_SIGN_UP,
+  SHOW_UPDATE_PASSWORD,
 }
 
-class MyApp extends StatelessWidget {
+void main() {
+  runApp(CancerApp());
+}
+
+class CancerApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Thesis Cancer',
       theme: ThemeData(
         // This is the theme of your application.
         //
@@ -22,13 +36,13 @@ class MyApp extends StatelessWidget {
         // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: CancerHomePage(title: 'Thesis Cancer Home Page'),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
+class CancerHomePage extends StatefulWidget {
+  CancerHomePage({Key key, this.title}) : super(key: key);
 
   // This widget is the home page of your application. It is stateful, meaning
   // that it has a State object (defined below) that contains fields that affect
@@ -42,72 +56,102 @@ class MyHomePage extends StatefulWidget {
   final String title;
 
   @override
-  _MyHomePageState createState() => _MyHomePageState();
+  _CancerHomePageState createState() => _CancerHomePageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class _CancerHomePageState extends State<CancerHomePage> {
+  bool _amplifyConfigured = false;
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+  final emailController = TextEditingController();
+  final phoneNumberController = TextEditingController();
+  final passwordController = TextEditingController();
+
+  bool isSignUpComplete = false;
+  bool isSignedIn = false;
+
+  @override
+  initState() {
+    super.initState();
+    _configureAmplify();
+  }
+
+  @override
+  void dispose() {
+    emailController.dispose();
+    // phoneNumberController.dispose();
+    passwordController.dispose();
+    super.dispose();
+  }
+
+  void _configureAmplify() async {
+    if (!mounted) return;
+
+    AmplifyAuthCognito authPlugin = AmplifyAuthCognito();
+    Amplify.addPlugins([authPlugin]);
+
+    try {
+      await Amplify.configure(amplifyconfig);
+      setState(() {
+        _amplifyConfigured = true;
+      });
+    } on AmplifyAlreadyConfiguredException {
+      print(
+          "Tried to reconfigure Amplify; this can occur when your app restarts on Android.");
+    } catch (e) {
+      print(e);
+    }
+  }
+
+  Future<String> _registerUser(LoginData data) async {
+    try {
+      Map<String, String> userAttributes = {
+        "email": emailController.text,
+      // 'phone_number': phoneNumberController.text,
+      };
+      SignUpResult res = await Amplify.Auth.signUp(
+          username: data.name,
+          password: data.password,
+          options: CognitoSignUpOptions(userAttributes: userAttributes));
+      setState(() {
+        isSignUpComplete = res.isSignUpComplete;
+        print("Sign up" + (isSignUpComplete ? "Complete" : "Not Complete"));
+      });
+    } on AuthException catch (e) {
+      print(e);
+      return "Register Error: " + e.toString();
+    }
+  }
+
+  Future<String> _signIn(LoginData data) async {
+    try {} on AuthException catch (e) {
+      print(e);
+      AlertDialog(
+        title: Text("Login Failed"),
+        content: SingleChildScrollView(
+          child: ListBody(
+            children: [Text(e.message)],
+          ),
+        ),
+        actions: <Widget>[
+          TextButton(
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+              child: Text('OK'))
+        ],
+      );
+      return 'Log In Error: ' + e.toString();
+    }
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+    return SafeArea(
+      child: FlutterLogin(
+          onLogin: _signIn,
+          onSignup: _registerUser,
+          onRecoverPassword: (_) => null,
+          title: 'Flutter Amplify'),
     );
   }
 }
