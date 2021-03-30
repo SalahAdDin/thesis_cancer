19195914c8ca2e2e637460a090a5cc3d0d1136d3
Luis Alaguna <alagunasalahaddin@live.com>
2021-03-30 22:35:18 +0300

    :fire: updating User entity.

--- lib/user/domain/user.entity.dart
+++ lib/user/domain/user.entity.dart
@@ -1,18 +1,26 @@
+import 'package:thesis_cancer/utils/types.dart';
+
 class User {
   final String id;
   final String email;
   final String displayName;
-  final String bio;
-  final String photoID;
-  final Map gallery;
-  final Map posts;
-  final Map comments;
-  final Map likes;
+  final UserRole role;
+  final bool? hasSeenTutorial;
+  final bool? hasSeenIntroductoryVideo;
+  final String? bio;
+  final String? photoID;
+  final Map? gallery;
+  final Map? posts;
+  final Map? comments;
+  final Map? likes;
 
   const User(
-      {this.id,
-      this.email,
-      this.displayName,
+      {required this.id,
+      required this.email,
+      required this.displayName,
+      required this.role,
+      this.hasSeenTutorial = false,
+      this.hasSeenIntroductoryVideo = false,
       this.bio,
       this.photoID,
       this.gallery,
