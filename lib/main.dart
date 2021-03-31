c26b7ddfdc6fbb4090a4001afdf97f57e4fc03cb
Luis Alaguna <alagunasalahaddin@live.com>
2021-03-31 21:48:16 +0300

    :tada: Configurating Amplify plugin for new architecture.
    - Requires test(it will be done in Login).

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,6 @@
+import 'package:amplify_api/amplify_api.dart';
+import 'package:amplify_auth_cognito/amplify_auth_cognito.dart';
+import 'package:amplify_flutter/amplify.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
@@ -6,19 +9,29 @@ import 'package:thesis_cancer/home/infraestructure/datastore.repository.dart';
 import 'package:thesis_cancer/home/presentation/pages/splash.dart';
 import 'package:thesis_cancer/provider.dart';
 
-enum displayState {
-  SIGNED_IN,
-  SHOW_CONFIRM,
-  SHOW_CONFIRM_RESET,
-  SHOW_CONFIRM_SIGN_IN,
-  SHOW_SIGN_IN,
-  SHOW_SIGN_UP,
-  SHOW_UPDATE_PASSWORD,
-}
+import 'amplifyconfiguration.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  AmplifyAuthCognito authPlugin = AmplifyAuthCognito();
+  AmplifyAPI apiPlugin = AmplifyAPI();
+  // AmplifyStorageS3 storageS3Plugin = AmplifyStorageS3();
+  // AmplifyAnalyticsPinpoint analyticsPinpointPlugin = AmplifyAnalyticsPinpoint();
+  Amplify.addPlugins([
+    authPlugin, apiPlugin,
+    //storageS3Plugin,
+    //analyticsPinpointPlugin
+  ]);
+
   final dataStore = await SembastDataStore.makeDefault();
+  try {
+    await Amplify.configure(amplifyconfig);
+  } on AmplifyAlreadyConfiguredException {
+    print(
+        "Tried to reconfigure Amplify; this can occur when your app restarts on Android.");
+  } catch (e) {
+    print(e);
+  }
   runApp(ProviderScope(
       overrides: [dataStoreRepositoryProvider.overrideWithValue(dataStore)],
       child: CancerApp()));
