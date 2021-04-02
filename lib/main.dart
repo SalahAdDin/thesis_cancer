0616cee27c240a7945ed84cac2c2f616a808587f
Luis Alaguna <alagunasalahaddin@live.com>
2021-04-02 15:08:35 +0300

    :fire: typo fixing.

--- lib/main.dart
+++ lib/main.dart
@@ -5,7 +5,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
-import 'package:thesis_cancer/home/infraestructure/datastore.repository.dart';
+import 'package:thesis_cancer/home/infrastructure/datastore.repository.dart';
 import 'package:thesis_cancer/home/presentation/pages/splash.dart';
 import 'package:thesis_cancer/provider.dart';
 
