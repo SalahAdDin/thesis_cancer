f579bb3676c8346704b760c65743bddff1621e1e
Luis Alaguna <alagunasalahaddin@live.com>
2021-05-21 18:35:50 +0300

    :fire: Updating routes.

--- lib/core/application/routes/router.dart
+++ lib/core/application/routes/router.dart
@@ -4,23 +4,25 @@ import 'package:thesis_cancer/features/auth/presentation/pages/login_screen.dart
 import 'package:thesis_cancer/features/home/presentation/pages/home_screen.dart';
 import 'package:thesis_cancer/features/home/presentation/pages/introductory_screen.dart';
 import 'package:thesis_cancer/features/home/presentation/pages/knowledge_screen.dart';
+import 'package:thesis_cancer/features/home/presentation/pages/lobby_screen.dart';
 import 'package:thesis_cancer/features/home/presentation/pages/research_screen.dart';
+import 'package:thesis_cancer/features/home/presentation/pages/splash_screen.dart';
 import 'package:thesis_cancer/features/home/presentation/pages/stories_screen.dart';
 import 'package:thesis_cancer/features/home/presentation/pages/therapy_screen.dart';
-import 'package:thesis_cancer/features/notification/presentation/pages/notifications_page.dart';
+import 'package:thesis_cancer/features/notification/presentation/pages/notifications_screen.dart';
 import 'package:thesis_cancer/features/survey/presentation/pages/survey_screen.dart';
 
 @MaterialAutoRouter(
   replaceInRouteName: 'Screen,Route',
   routes: <AutoRoute>[
     AutoRoute(path: '/login', page: LoginScreen),
-    RedirectRoute(path: '/logout', redirectTo: ''),
+    RedirectRoute(path: '/logout', redirectTo: '/'),
+    RedirectRoute(path: '*', redirectTo: '/'),
     AutoRoute(
         path: '/',
         // TODO: should this be removed
         page: HomeScreen,
-        usesTabsRouter: true,
-        guards: [AuthGuard],
+        // guards: [AuthGuard],
         // TODO: Initial just for non admin users
         initial: true,
         children: [
@@ -29,8 +31,10 @@ import 'package:thesis_cancer/features/survey/presentation/pages/survey_screen.d
           AutoRoute(path: 'research', page: ResearchScreen),
           AutoRoute(path: 'stories', page: StoriesScreen),
         ]),
+    AutoRoute(path: "/splash", page: SplashScreen),
+    AutoRoute(path: "/lobby", page: LobbyScreen),
     AutoRoute(path: "/introduction", page: IntroductoryScreen),
-    AutoRoute(path: '/notifications', page: NotificationsPage),
+    AutoRoute(path: '/notifications', page: NotificationsScreen),
     AutoRoute(path: '/surveys/:id', page: SurveyScreen),
     /*AutoRoute(
         path: '/users', name: 'UsersRouter', page: EmptyRouterPage, children: [
--- lib/core/application/routes/router.gr.dart
+++ lib/core/application/routes/router.gr.dart
@@ -7,190 +7,242 @@
 import 'package:auto_route/auto_route.dart' as _i1;
 import 'package:flutter/material.dart' as _i2;
 
-import '../../../features/auth/presentation/pages/login.dart' as _i4;
-import '../../../features/home/presentation/pages/home_screen.dart' as _i5;
+import '../../../features/auth/presentation/pages/login_screen.dart' as _i3;
+import '../../../features/home/presentation/pages/home_screen.dart' as _i4;
 import '../../../features/home/presentation/pages/introductory_screen.dart'
-    as _i6;
-import '../../../features/home/presentation/pages/knowledge_screen.dart' as _i8;
-import '../../../features/home/presentation/pages/research_screen.dart' as _i10;
-import '../../../features/home/presentation/pages/stories_screen.dart' as _i11;
-import '../../../features/home/presentation/pages/therapy_screen.dart' as _i9;
-import '../../../features/survey/presentation/pages/survey_screen.dart' as _i7;
-import 'auth.guard.dart' as _i3;
+    as _i7;
+import '../../../features/home/presentation/pages/knowledge_screen.dart'
+    as _i10;
+import '../../../features/home/presentation/pages/lobby_screen.dart' as _i6;
+import '../../../features/home/presentation/pages/research_screen.dart' as _i12;
+import '../../../features/home/presentation/pages/splash_screen.dart' as _i5;
+import '../../../features/home/presentation/pages/stories_screen.dart' as _i13;
+import '../../../features/home/presentation/pages/therapy_screen.dart' as _i11;
+import '../../../features/notification/presentation/pages/notifications_screen.dart'
+    as _i8;
+import '../../../features/survey/presentation/pages/survey_screen.dart' as _i9;
 
 class AppRouter extends _i1.RootStackRouter {
-  AppRouter(
-      {_i2.GlobalKey<_i2.NavigatorState>? navigatorKey,
-      required this.authGuard})
+  AppRouter([_i2.GlobalKey<_i2.NavigatorState>? navigatorKey])
       : super(navigatorKey);
-
-  final _i3.AuthGuard authGuard;
 
   @override
   final Map<String, _i1.PageFactory> pagesMap = {
-    LoginScreen.name: (routeData) {
-      return _i1.MaterialPageX<dynamic>(
-          routeData: routeData, child: _i4.LoginScreen());
-    },
-    HomeScreen.name: (routeData) {
-      final args = routeData.argsAs<HomeScreenArgs>(
-          orElse: () => const HomeScreenArgs());
-      return _i1.MaterialPageX<dynamic>(
-          routeData: routeData, child: _i5.HomeScreen(key: args.key));
-    },
-    IntroductoryScreen.name: (routeData) {
-      final args = routeData.argsAs<IntroductoryScreenArgs>(
-          orElse: () => const IntroductoryScreenArgs());
-      return _i1.MaterialPageX<dynamic>(
-          routeData: routeData, child: _i6.IntroductoryScreen(key: args.key));
-    },
-    SurveyScreen.name: (routeData) {
-      final args = routeData.argsAs<SurveyScreenArgs>();
-      return _i1.MaterialPageX<dynamic>(
-          routeData: routeData,
-          child: _i7.SurveyScreen(onCompleteSurvey: args.onCompleteSurvey));
-    },
-    KnowledgeScreen.name: (routeData) {
-      final args = routeData.argsAs<KnowledgeScreenArgs>(
-          orElse: () => const KnowledgeScreenArgs());
-      return _i1.MaterialPageX<dynamic>(
-          routeData: routeData, child: _i8.KnowledgeScreen(key: args.key));
-    },
-    TherapyScreen.name: (routeData) {
-      final args = routeData.argsAs<TherapyScreenArgs>(
-          orElse: () => const TherapyScreenArgs());
-      return _i1.MaterialPageX<dynamic>(
-          routeData: routeData, child: _i9.TherapyScreen(key: args.key));
-    },
-    ResearchScreen.name: (routeData) {
-      final args = routeData.argsAs<ResearchScreenArgs>(
-          orElse: () => const ResearchScreenArgs());
-      return _i1.MaterialPageX<dynamic>(
-          routeData: routeData, child: _i10.ResearchScreen(key: args.key));
-    },
-    StoriesScreen.name: (routeData) {
-      final args = routeData.argsAs<StoriesScreenArgs>(
-          orElse: () => const StoriesScreenArgs());
-      return _i1.MaterialPageX<dynamic>(
-          routeData: routeData, child: _i11.StoriesScreen(key: args.key));
-    }
+    LoginRoute.name: (routeData) => _i1.MaterialPageX<dynamic>(
+        routeData: routeData,
+        builder: (_) {
+          return _i3.LoginScreen();
+        }),
+    HomeRoute.name: (routeData) => _i1.MaterialPageX<dynamic>(
+        routeData: routeData,
+        builder: (data) {
+          final args =
+              data.argsAs<HomeRouteArgs>(orElse: () => const HomeRouteArgs());
+          return _i4.HomeScreen(key: args.key);
+        }),
+    SplashRoute.name: (routeData) => _i1.MaterialPageX<dynamic>(
+        routeData: routeData,
+        builder: (_) {
+          return _i5.SplashScreen();
+        }),
+    LobbyRoute.name: (routeData) => _i1.MaterialPageX<dynamic>(
+        routeData: routeData,
+        builder: (_) {
+          return _i6.LobbyScreen();
+        }),
+    IntroductoryRoute.name: (routeData) => _i1.MaterialPageX<dynamic>(
+        routeData: routeData,
+        builder: (data) {
+          final args = data.argsAs<IntroductoryRouteArgs>(
+              orElse: () => const IntroductoryRouteArgs());
+          return _i7.IntroductoryScreen(key: args.key);
+        }),
+    NotificationsRoute.name: (routeData) => _i1.MaterialPageX<dynamic>(
+        routeData: routeData,
+        builder: (_) {
+          return _i8.NotificationsScreen();
+        }),
+    SurveyRoute.name: (routeData) => _i1.MaterialPageX<dynamic>(
+        routeData: routeData,
+        builder: (data) {
+          final args = data.argsAs<SurveyRouteArgs>();
+          return _i9.SurveyScreen(
+              onCompleteSurvey: args.onCompleteSurvey, surveyID: args.surveyID);
+        }),
+    KnowledgeRoute.name: (routeData) => _i1.MaterialPageX<dynamic>(
+        routeData: routeData,
+        builder: (data) {
+          final args = data.argsAs<KnowledgeRouteArgs>(
+              orElse: () => const KnowledgeRouteArgs());
+          return _i10.KnowledgeScreen(key: args.key);
+        }),
+    TherapyRoute.name: (routeData) => _i1.MaterialPageX<dynamic>(
+        routeData: routeData,
+        builder: (data) {
+          final args = data.argsAs<TherapyRouteArgs>(
+              orElse: () => const TherapyRouteArgs());
+          return _i11.TherapyScreen(key: args.key);
+        }),
+    ResearchRoute.name: (routeData) => _i1.MaterialPageX<dynamic>(
+        routeData: routeData,
+        builder: (data) {
+          final args = data.argsAs<ResearchRouteArgs>(
+              orElse: () => const ResearchRouteArgs());
+          return _i12.ResearchScreen(key: args.key);
+        }),
+    StoriesRoute.name: (routeData) => _i1.MaterialPageX<dynamic>(
+        routeData: routeData,
+        builder: (data) {
+          final args = data.argsAs<StoriesRouteArgs>(
+              orElse: () => const StoriesRouteArgs());
+          return _i13.StoriesScreen(key: args.key);
+        })
   };
 
   @override
   List<_i1.RouteConfig> get routes => [
-        _i1.RouteConfig(LoginScreen.name, path: '/login'),
+        _i1.RouteConfig(LoginRoute.name, path: '/login'),
         _i1.RouteConfig('/logout#redirect',
-            path: '/logout', redirectTo: '', fullMatch: true),
-        _i1.RouteConfig(HomeScreen.name, path: '/', guards: [
-          authGuard
-        ], children: [
-          _i1.RouteConfig(KnowledgeScreen.name, path: 'knowledge'),
-          _i1.RouteConfig(TherapyScreen.name, path: 'therapy'),
-          _i1.RouteConfig(ResearchScreen.name, path: 'research'),
-          _i1.RouteConfig(StoriesScreen.name, path: 'stories')
+            path: '/logout', redirectTo: '/', fullMatch: true),
+        _i1.RouteConfig('*#redirect',
+            path: '*', redirectTo: '/', fullMatch: true),
+        _i1.RouteConfig(HomeRoute.name, path: '/', children: [
+          _i1.RouteConfig(KnowledgeRoute.name, path: 'knowledge'),
+          _i1.RouteConfig(TherapyRoute.name, path: 'therapy'),
+          _i1.RouteConfig(ResearchRoute.name, path: 'research'),
+          _i1.RouteConfig(StoriesRoute.name, path: 'stories')
         ]),
-        _i1.RouteConfig(IntroductoryScreen.name, path: '/introduction'),
-        _i1.RouteConfig(SurveyScreen.name, path: '/surveys/:id')
+        _i1.RouteConfig(SplashRoute.name, path: '/splash'),
+        _i1.RouteConfig(LobbyRoute.name, path: '/lobby'),
+        _i1.RouteConfig(IntroductoryRoute.name, path: '/introduction'),
+        _i1.RouteConfig(NotificationsRoute.name, path: '/notifications'),
+        _i1.RouteConfig(SurveyRoute.name, path: '/surveys/:id')
       ];
 }
 
-class LoginScreen extends _i1.PageRouteInfo {
-  const LoginScreen() : super(name, path: '/login');
+class LoginRoute extends _i1.PageRouteInfo {
+  const LoginRoute() : super(name, path: '/login');
 
-  static const String name = 'LoginScreen';
+  static const String name = 'LoginRoute';
 }
 
-class HomeScreen extends _i1.PageRouteInfo<HomeScreenArgs> {
-  HomeScreen({_i2.Key? key, List<_i1.PageRouteInfo>? children})
+class HomeRoute extends _i1.PageRouteInfo<HomeRouteArgs> {
+  HomeRoute({_i2.Key? key, List<_i1.PageRouteInfo>? children})
       : super(name,
-            path: '/', args: HomeScreenArgs(key: key), children: children);
+            path: '/',
+            args: HomeRouteArgs(key: key),
+            initialChildren: children);
 
-  static const String name = 'HomeScreen';
+  static const String name = 'HomeRoute';
 }
 
-class HomeScreenArgs {
-  const HomeScreenArgs({this.key});
+class HomeRouteArgs {
+  const HomeRouteArgs({this.key});
 
   final _i2.Key? key;
 }
 
-class IntroductoryScreen extends _i1.PageRouteInfo<IntroductoryScreenArgs> {
-  IntroductoryScreen({_i2.Key? key})
-      : super(name,
-            path: '/introduction', args: IntroductoryScreenArgs(key: key));
+class SplashRoute extends _i1.PageRouteInfo {
+  const SplashRoute() : super(name, path: '/splash');
 
-  static const String name = 'IntroductoryScreen';
+  static const String name = 'SplashRoute';
 }
 
-class IntroductoryScreenArgs {
-  const IntroductoryScreenArgs({this.key});
+class LobbyRoute extends _i1.PageRouteInfo {
+  const LobbyRoute() : super(name, path: '/lobby');
+
+  static const String name = 'LobbyRoute';
+}
+
+class IntroductoryRoute extends _i1.PageRouteInfo<IntroductoryRouteArgs> {
+  IntroductoryRoute({_i2.Key? key})
+      : super(name,
+            path: '/introduction', args: IntroductoryRouteArgs(key: key));
+
+  static const String name = 'IntroductoryRoute';
+}
+
+class IntroductoryRouteArgs {
+  const IntroductoryRouteArgs({this.key});
 
   final _i2.Key? key;
 }
 
-class SurveyScreen extends _i1.PageRouteInfo<SurveyScreenArgs> {
-  SurveyScreen({required void Function() onCompleteSurvey})
+class NotificationsRoute extends _i1.PageRouteInfo {
+  const NotificationsRoute() : super(name, path: '/notifications');
+
+  static const String name = 'NotificationsRoute';
+}
+
+class SurveyRoute extends _i1.PageRouteInfo<SurveyRouteArgs> {
+  SurveyRoute(
+      {required void Function() onCompleteSurvey, required String surveyID})
       : super(name,
             path: '/surveys/:id',
-            args: SurveyScreenArgs(onCompleteSurvey: onCompleteSurvey));
+            args: SurveyRouteArgs(
+                onCompleteSurvey: onCompleteSurvey, surveyID: surveyID),
+            rawPathParams: {'surveyID': surveyID});
 
-  static const String name = 'SurveyScreen';
+  static const String name = 'SurveyRoute';
 }
 
-class SurveyScreenArgs {
-  const SurveyScreenArgs({required this.onCompleteSurvey});
+class SurveyRouteArgs {
+  const SurveyRouteArgs(
+      {required this.onCompleteSurvey, required this.surveyID});
 
   final void Function() onCompleteSurvey;
+
+  final String surveyID;
 }
 
-class KnowledgeScreen extends _i1.PageRouteInfo<KnowledgeScreenArgs> {
-  KnowledgeScreen({_i2.Key? key})
-      : super(name, path: 'knowledge', args: KnowledgeScreenArgs(key: key));
+class KnowledgeRoute extends _i1.PageRouteInfo<KnowledgeRouteArgs> {
+  KnowledgeRoute({_i2.Key? key})
+      : super(name, path: 'knowledge', args: KnowledgeRouteArgs(key: key));
 
-  static const String name = 'KnowledgeScreen';
+  static const String name = 'KnowledgeRoute';
 }
 
-class KnowledgeScreenArgs {
-  const KnowledgeScreenArgs({this.key});
+class KnowledgeRouteArgs {
+  const KnowledgeRouteArgs({this.key});
 
   final _i2.Key? key;
 }
 
-class TherapyScreen extends _i1.PageRouteInfo<TherapyScreenArgs> {
-  TherapyScreen({_i2.Key? key})
-      : super(name, path: 'therapy', args: TherapyScreenArgs(key: key));
+class TherapyRoute extends _i1.PageRouteInfo<TherapyRouteArgs> {
+  TherapyRoute({_i2.Key? key})
+      : super(name, path: 'therapy', args: TherapyRouteArgs(key: key));
 
-  static const String name = 'TherapyScreen';
+  static const String name = 'TherapyRoute';
 }
 
-class TherapyScreenArgs {
-  const TherapyScreenArgs({this.key});
+class TherapyRouteArgs {
+  const TherapyRouteArgs({this.key});
 
   final _i2.Key? key;
 }
 
-class ResearchScreen extends _i1.PageRouteInfo<ResearchScreenArgs> {
-  ResearchScreen({_i2.Key? key})
-      : super(name, path: 'research', args: ResearchScreenArgs(key: key));
+class ResearchRoute extends _i1.PageRouteInfo<ResearchRouteArgs> {
+  ResearchRoute({_i2.Key? key})
+      : super(name, path: 'research', args: ResearchRouteArgs(key: key));
 
-  static const String name = 'ResearchScreen';
+  static const String name = 'ResearchRoute';
 }
 
-class ResearchScreenArgs {
-  const ResearchScreenArgs({this.key});
+class ResearchRouteArgs {
+  const ResearchRouteArgs({this.key});
 
   final _i2.Key? key;
 }
 
-class StoriesScreen extends _i1.PageRouteInfo<StoriesScreenArgs> {
-  StoriesScreen({_i2.Key? key})
-      : super(name, path: 'stories', args: StoriesScreenArgs(key: key));
+class StoriesRoute extends _i1.PageRouteInfo<StoriesRouteArgs> {
+  StoriesRoute({_i2.Key? key})
+      : super(name, path: 'stories', args: StoriesRouteArgs(key: key));
 
-  static const String name = 'StoriesScreen';
+  static const String name = 'StoriesRoute';
 }
 
-class StoriesScreenArgs {
-  const StoriesScreenArgs({this.key});
+class StoriesRouteArgs {
+  const StoriesRouteArgs({this.key});
 
   final _i2.Key? key;
 }
