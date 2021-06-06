04108a6ece166507a32d40d20cf1196b4cdd1cdc
Luis Alaguna <alagunasalahaddin@live.com>
2021-06-06 13:07:23 +0300

    :tada: Adding description row to the post.

--- lib/features/content/presentation/widgets/post_widget.dart
+++ lib/features/content/presentation/widgets/post_widget.dart
@@ -5,6 +5,7 @@ import 'package:card_swiper/card_swiper.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:like_button/like_button.dart';
+import 'package:readmore/readmore.dart';
 import 'package:thesis_cancer/features/content/domain/post/post.entity.dart';
 
 class PostWidget extends StatelessWidget {
@@ -19,21 +20,23 @@ class PostWidget extends StatelessWidget {
   Widget build(BuildContext context) {
     final double swiperWidth = MediaQuery.of(context).size.width - 60;
     final double swiperHeight = swiperWidth * 3 / 4;
-    return Column(
-      mainAxisSize: MainAxisSize.min,
-      children: <Widget>[
-        // PostHeader(),
-        GestureDetector(
-            // TODO: do like on post
-            onDoubleTap: () => null,
-            child: SizedBox(
-                height: swiperHeight,
-                child: Swiper(
-                  itemCount: post.gallery.length,
-                  itemHeight: swiperHeight,
-                  itemWidth: swiperWidth,
-                  pagination: const SwiperPagination(),
-                  /*pagination: SwiperPagination(
+
+    return SingleChildScrollView(
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: <Widget>[
+          // PostHeader(),
+          GestureDetector(
+              // TODO: do like on post
+              onDoubleTap: () => null,
+              child: SizedBox(
+                  height: swiperHeight,
+                  child: Swiper(
+                    itemCount: post.gallery.length,
+                    itemHeight: swiperHeight,
+                    itemWidth: swiperWidth,
+                    pagination: const SwiperPagination(),
+                    /*pagination: SwiperPagination(
                     builder: SwiperCustomPagination(
                       builder:
                           (BuildContext context, SwiperPluginConfig? config) =>
@@ -43,23 +46,23 @@ class PostWidget extends StatelessWidget {
                       ).build(context, config),
                     ),
                   ),*/
-                  itemBuilder: (_, int index) => CachedNetworkImage(
-                    imageUrl: post.gallery[index].url,
-                    fit: BoxFit.fill,
-                    progressIndicatorBuilder:
-                        (context, url, downloadProgress) => SizedBox(
-                      height: 400.0,
-                      child: Center(
-                        child: CircularProgressIndicator(
-                          value: downloadProgress.progress,
+                    itemBuilder: (_, int index) => CachedNetworkImage(
+                      imageUrl: post.gallery[index].url,
+                      fit: BoxFit.fill,
+                      progressIndicatorBuilder:
+                          (context, url, downloadProgress) => SizedBox(
+                        height: 400.0,
+                        child: Center(
+                          child: CircularProgressIndicator(
+                            value: downloadProgress.progress,
+                          ),
                         ),
                       ),
+                      errorWidget: (context, url, error) =>
+                          const Icon(Icons.error),
                     ),
-                    errorWidget: (context, url, error) =>
-                        const Icon(Icons.error),
-                  ),
-                ))
-            /*CarouselSlider(
+                  ))
+              /*CarouselSlider(
             items: post.gallery
                 .map(
                   (element) => CachedNetworkImage(
@@ -81,40 +84,56 @@ class PostWidget extends StatelessWidget {
                 .toList(),
             options: CarouselOptions(height: swiperHeight, aspectRatio: 4 / 3),
           ),*/
-            ),
-        Padding(
-          padding: const EdgeInsets.all(16.0),
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: <Widget>[
-                  const LikeButton(),
-                  const SizedBox(
-                    width: 10,
-                  ),
-                  const Icon(
-                    Icons.maps_ugc_rounded,
-                  ),
-                  const SizedBox(
-                    width: 10,
-                  ),
-                  Transform.rotate(
-                    angle: -45 * pi / 180,
-                    child: const Icon(
-                      Icons.send_rounded,
+              ),
+          Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: <Widget>[
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: <Widget>[
+                    const LikeButton(),
+                    const SizedBox(
+                      width: 10,
                     ),
-                  ),
-                ],
-              ),
-              const Icon(
-                Icons.bookmark_border_rounded,
-              ),
-            ],
+                    const Icon(
+                      Icons.maps_ugc_rounded,
+                    ),
+                    const SizedBox(
+                      width: 10,
+                    ),
+                    Transform.rotate(
+                      angle: -45 * pi / 180,
+                      child: const Icon(
+                        Icons.send_rounded,
+                      ),
+                    ),
+                  ],
+                ),
+                const Icon(
+                  Icons.bookmark_border_rounded,
+                ),
+              ],
+            ),
           ),
-        ),
-      ],
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 16.0),
+            child: ReadMoreText(
+              post.description,
+              colorClickableText: Colors.pink,
+              trimMode: TrimMode.Line,
+              trimCollapsedText: '...more',
+              trimExpandedText: '...less',
+              moreStyle: const TextStyle(
+                  fontSize: 14,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.grey),
+              style: Theme.of(context).textTheme.bodyText1,
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
