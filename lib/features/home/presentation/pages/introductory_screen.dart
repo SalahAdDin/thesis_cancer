d0e8134be670686f5aff99ef2b683a605de70f8b
Luis Alaguna <alagunasalahaddin@live.com>
2021-06-20 01:50:41 +0300

    :fire: Disable done button till the video is finished.

--- lib/features/home/presentation/pages/introductory_screen.dart
+++ lib/features/home/presentation/pages/introductory_screen.dart
@@ -1,22 +1,45 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:introduction_screen/introduction_screen.dart';
 import 'package:thesis_cancer/core/domain/types.dart';
 import 'package:thesis_cancer/core/presentation/widgets/video_item.dart';
 import 'package:video_player/video_player.dart';
 
-class IntroductoryScreen extends StatelessWidget {
+/// Introductory Screen
+class IntroductoryScreen extends HookWidget {
+  ///
   const IntroductoryScreen({
     Key? key,
     required this.onDone,
     required this.dataSource,
   }) : super(key: key);
 
+  ///
   final String dataSource;
+
+  ///
   final OnPressedButton onDone;
 
   @override
   Widget build(BuildContext context) {
+    final VideoPlayerController _videoPlayerController =
+        VideoPlayerController.network(dataSource);
+    final ValueNotifier<bool> _isVideoFinished = useState(false);
+
+    useEffect(() {
+      void checkIsFinished() {
+        _isVideoFinished.value = _videoPlayerController.value.isInitialized &&
+            _videoPlayerController.value.position ==
+                _videoPlayerController.value.duration;
+      }
+
+      _videoPlayerController.addListener(checkIsFinished);
+      return () {
+        _videoPlayerController.removeListener(checkIsFinished);
+      };
+    }, <VideoPlayerController>[_videoPlayerController]);
+
     return Scaffold(
       resizeToAvoidBottomInset: false,
       body: IntroductionScreen(
@@ -24,14 +47,16 @@ class IntroductoryScreen extends StatelessWidget {
           PageViewModel(
             title: "",
             image: VideoItem(
-              videoPlayerController: VideoPlayerController.network(dataSource),
-              autoplay: true,
+              videoPlayerController: _videoPlayerController,
+              autoPlay: true,
             ),
             body: "",
             decoration: const PageDecoration(fullScreen: true),
           ),
         ],
         done: const Text("Done", style: TextStyle(fontWeight: FontWeight.w600)),
+        showDoneButton: _isVideoFinished.value,
+        isProgress: false,
         onDone: onDone,
         showNextButton: false,
       ),
