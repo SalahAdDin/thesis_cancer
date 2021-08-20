8115b34dd373cf4e9af1af77319c08140f9ee8bf
Luis <alagunasalahaddin@live.com>
2021-08-20 16:44:47 +0300

    :fire: removed ununused provider.
    - i forgot to remove in the better-player branch.

--- lib/features/media/application/uploadfile.provider.dart
+++ lib/features/media/application/uploadfile.provider.dart
@@ -2,7 +2,6 @@ import 'package:flutter_cache_manager/flutter_cache_manager.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:thesis_cancer/features/media/domain/uploadfile.repository.dart';
 import 'package:thesis_cancer/features/media/infrastructure/uploadfile.repository.dart';
-import 'package:video_player/video_player.dart';
 
 final Provider<UploadFileRepository> uploadFileRepositoryProvider =
     Provider<UploadFileRepository>(
@@ -15,24 +14,3 @@ final Provider<CacheManager> cacheManagerProvider = Provider<CacheManager>(
   (_) => DefaultCacheManager(),
   name: "Default Cache Manager Provider",
 );
-
-///
-final FutureProviderFamily<VideoPlayerController, String>
-    videoControllerProvider =
-    FutureProvider.family<VideoPlayerController, String>(
-        (ProviderReference ref, String url) async {
-  final CacheManager _cacheManager = ref.read(cacheManagerProvider);
-
-  final FileInfo? fileInfo = await _cacheManager.getFileFromCache(url);
-
-  if (fileInfo == null) {
-    print('[VideoControllerService]: No video in cache.');
-
-    print('[VideoControllerService]: Saving video to cache.');
-    _cacheManager.downloadFile(url);
-    return VideoPlayerController.network(url);
-  } else {
-    print('[VideoControllerService]: Loading video from cache.');
-    return VideoPlayerController.file(fileInfo.file);
-  }
-}, name: "Video Controller Provider");
