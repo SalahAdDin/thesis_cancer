b68761a9a3f47870d1741099ff1de8c72656800f
Luis Alaguna <alagunasalahaddin@live.com>
2021-12-18 21:28:43 +0300

    :fire: improving the widget.

--- lib/core/presentation/widgets/read_more_markdown.dart
+++ lib/core/presentation/widgets/read_more_markdown.dart
@@ -1,8 +1,13 @@
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_markdown/flutter_markdown.dart';
 import 'package:url_launcher/url_launcher.dart';
 
 class ReadMoreMarkdown extends StatefulWidget {
+  // TODO: handling trim text in the best way
+  // TODO: Animation at expanding/collapsing
+  // TODO: Parse markdown to avoid breaking at trimming
+  // Base: https://github.com/jonataslaw/readmore/blob/master/lib/readmore.dart
   const ReadMoreMarkdown({
     Key? key,
     this.trimLength = 240,
@@ -24,44 +29,50 @@ class ReadMoreMarkdown extends StatefulWidget {
 }
 
 class _ReadMoreMarkdownState extends State<ReadMoreMarkdown> {
-  bool showMore = false;
+  bool _readMore = false;
 
   Future<void> _launchURL(String url) async =>
       await canLaunch(url) ? launch(url) : throw 'Could not launch $url';
 
-  void _onClickShowMore() {
+  void _onTapLink() {
     setState(() {
-      showMore = !showMore;
+      _readMore = !_readMore;
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    return Stack(
-      children: <Widget>[
-        MarkdownBody(
-          data: showMore
-              ? widget.data
-              : widget.data.substring(
-                  0,
-                  widget.trimLength,
-                ),
-          selectable: true,
-          onTapLink: (String text, String? href, String title) async {
-            if (href != null) await _launchURL(href);
-          },
-        ),
-        Positioned(
-          right: 0.0,
-          bottom: -17.5,
-          child: TextButton(
-            onPressed: _onClickShowMore,
-            child: Text(
-              showMore ? widget.trimExpandedText : widget.trimCollapsedText,
+    final TextSpan link = TextSpan(
+      text: _readMore ? widget.trimExpandedText : widget.trimCollapsedText,
+      // style: _readMore ? _defaultMoreStyle : _defaultLessStyle,
+      style: TextStyle(
+        color: Theme.of(context).primaryColor,
+        fontWeight: FontWeight.bold,
+      ),
+      recognizer: TapGestureRecognizer()..onTap = _onTapLink,
+    );
+
+    return RichText(
+      textAlign: TextAlign.end,
+      text: TextSpan(
+        children: <InlineSpan>[
+          WidgetSpan(
+            child: MarkdownBody(
+              data: _readMore
+                  ? widget.data
+                  : widget.data.substring(
+                      0,
+                      widget.trimLength,
+                    ),
+              selectable: true,
+              onTapLink: (String text, String? href, String title) async {
+                if (href != null) await _launchURL(href);
+              },
             ),
           ),
-        )
-      ],
+          link,
+        ],
+      ),
     );
   }
 }
