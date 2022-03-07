805c618411e87f5dee78691c48eb7f642c88869a
Luis Alaguna <alagunasalahaddin@live.com>
2022-03-07 16:29:23 +0300

    :fire: cleaning global constants.

--- lib/core/domain/constants.dart
+++ lib/core/domain/constants.dart
@@ -14,19 +14,16 @@ class AppLiterals {
       'Copyright ©2021 CloudNesil – Cloud Computing, Development Consulting, Cutting The Edge Technology.';
 }
 
+///
 class BackendURL {
-  static const String devURL = "http://172.16.0.63:1337/";
-  static const String testURL = "http://95.173.162.150:1337/";
-  static const String productionURL = "";
-}
+  ///
+  static const String devURL = "http://192.168.2.198:1337/";
 
-// CloudFront
-class CloudFrontPolicy {
-  static const String policy =
-      "eyJTdGF0ZW1lbnQiOlt7IlJlc291cmNlIjoiaHR0cCo6Ly9kazNrNGM1aHdjZzY1LmNsb3VkZnJvbnQubmV0LyoiLCJDb25kaXRpb24iOnsiRGF0ZUxlc3NUaGFuIjp7IkFXUzpFcG9jaFRpbWUiOjE2NzIzOTQzNDd9fX1dfQ__";
-  static const String keyPairId = "APKAITTLE5ROAARZLSKQ";
-  static const String signature =
-      "lM~RtbiwrOhIBvo5MBH52XCxExzwAyE-UyQKaUsuXI5rfz04X7noPhYn~QicfS0mRnmC4kB~6CK-8nxwp4JeV-sQuwPvxREHgpiNew8o3lAV0svv6FaAwTHDHbuJkLxVq7p70gWYEzfBJVWfgmeYzB946do~iwXlo-cjU2Dj94SBzY5bOThCBLELmpCZ273oDhIr~LuOYw2Kqebpsuta5gpqO3ki7GVux41-XrONT13M5zwadYrW4xFe2qleQQ-QF4s24k2eVr6xRv0AUGbfBp~wmC~7d4vz-hAhyezJ6CxXCevyGLSZzLTlDe1iM~Jd6UG-DieusIWeFUzvtwUj8g__";
+  ///
+  static const String testURL = "http://95.173.162.150:1337/";
+
+  ///
+  static const String productionURL = "";
 }
 
 /// Global key to identify UI elements
