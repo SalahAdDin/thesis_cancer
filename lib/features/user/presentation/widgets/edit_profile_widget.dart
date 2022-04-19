dccd424770b6a24024ba404c05ab51d3c123167b
Luis Alaguna <alagunasalahaddin@live.com>
2022-04-20 02:34:05 +0300

    :fire: removing unrequired package imports

--- lib/features/user/presentation/widgets/edit_profile_widget.dart
+++ lib/features/user/presentation/widgets/edit_profile_widget.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_form_builder/flutter_form_builder.dart';
-import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:form_builder_validators/form_builder_validators.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:mask_text_input_formatter/mask_text_input_formatter.dart';
--- lib/features/user/presentation/widgets/profile_widget.dart
+++ lib/features/user/presentation/widgets/profile_widget.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:flutter_markdown/flutter_markdown.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:thesis_cancer/core/presentation/widgets/user_avatar.dart';
