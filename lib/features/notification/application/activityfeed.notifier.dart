582f2fb31604d3bceb09f96f5d3647fe7a75c198
Luis Alaguna <alagunasalahaddin@live.com>
2022-05-24 13:56:58 +0300

    :tada: Improving Activity Notifier by adding deliver and request permission.

--- lib/features/notification/application/activityfeed.notifier.dart
+++ lib/features/notification/application/activityfeed.notifier.dart
@@ -2,21 +2,26 @@ import 'dart:async';
 
 import 'package:colorize/colorize.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/foundation.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:thesis_cancer/core/application/global.provider.dart';
 import 'package:thesis_cancer/core/application/launcher/launcher.notifier.dart';
+import 'package:thesis_cancer/core/application/navigator.dart';
 import 'package:thesis_cancer/core/application/settings.notifier.dart';
 import 'package:thesis_cancer/core/domain/types.dart';
 import 'package:thesis_cancer/features/notification/application/activityfeed.provider.dart';
 import 'package:thesis_cancer/features/notification/application/activityfeed.state.dart';
 import 'package:thesis_cancer/features/notification/domain/activityfeed.entity.dart';
 import 'package:thesis_cancer/features/notification/infrastructure/failure.dart';
+import 'package:thesis_cancer/features/survey/presentation/pages/survey_screen.dart';
 import 'package:thesis_cancer/features/user/application/user.provider.dart';
 import 'package:thesis_cancer/features/user/domain/profile.entity.dart';
 import 'package:thesis_cancer/features/user/domain/profile.repository.dart';
 import 'package:thesis_cancer/features/user/domain/user.entity.dart';
 import 'package:thesis_cancer/features/user/domain/user.repository.dart';
+import 'package:thesis_cancer/features/user/presentation/pages/profile_screen.dart';
+import 'package:uuid/uuid.dart';
 
 ///
 class ActivityFeedNotifier extends StateNotifier<ActivityFeedState> {
@@ -30,7 +35,11 @@ class ActivityFeedNotifier extends StateNotifier<ActivityFeedState> {
   ///
   final Reader reader;
 
-  Stream<RemoteMessage> get _feedProvider => reader(feedProvider.stream);
+  FirebaseMessaging get _firebaseMessaging =>
+      reader(firebaseMessagingInstanceProvider);
+
+  Stream<RemoteMessage> get _feedOnMessageProvider =>
+      FirebaseMessaging.onMessage;
 
   LauncherNotifier get _launcherProvider => reader(launcherProvider.notifier);
 
@@ -45,7 +54,7 @@ class ActivityFeedNotifier extends StateNotifier<ActivityFeedState> {
   List<ActivityFeed> feeds = <ActivityFeed>[];
 
   ///
-  int get count => feeds.where((ActivityFeed feed) => !feed.isRead).length;
+  int get feedsCount => feeds.where((ActivityFeed feed) => !feed.isRead).length;
 
   late StreamSubscription<RemoteMessage> _subscription;
 
@@ -53,6 +62,47 @@ class ActivityFeedNotifier extends StateNotifier<ActivityFeedState> {
   void dispose() {
     _subscription.cancel();
     super.dispose();
+  }
+
+  ///
+  Future<void> deliverFeedAction(
+    ActivityFeed feed,
+    NavigatorState navigatorState,
+  ) async {
+    switch (feed.type) {
+      case ActivityType.NEW_POST:
+        final PostType? postType = feed.data!["type"] as PostType?;
+        if (postType != null) {
+          // pushToPage(navigatorState, widget)
+        }
+        break;
+      case ActivityType.SCHEDULED_SURVEY_REMINDER:
+        final String? surveyId = feed.data!["id"] as String?;
+        if (surveyId != null) {
+          pushToPage(
+            navigatorState,
+            SurveyScreen(
+              onCompleteSurvey: () => navigatorState.pop(),
+              surveyID: surveyId,
+            ),
+          );
+        }
+        break;
+      case ActivityType.NEW_USER_REGISTERED:
+        final String? userId = feed.data!["id"] as String?;
+        if (userId != null) {
+          final User aimedUser = await _userRepository.findById(userId);
+
+          pushToPage(navigatorState, ProfileScreen(user: aimedUser));
+        }
+        break;
+      case ActivityType.SETTINGS_UPDATED:
+        _settingsController.getSettings();
+        break;
+      case ActivityType.USER_CONFIRMED:
+        _launcherProvider.signOut();
+        break;
+    }
   }
 
   ///
@@ -75,28 +125,38 @@ class ActivityFeedNotifier extends StateNotifier<ActivityFeedState> {
   }
 
   ///
-  Future<void> deliverFeedAction(ActivityFeed feed) async {
-    switch (feed.type) {
-      case ActivityType.NEW_POST:
-        final PostType postType = feed.data!["type"] as PostType;
-        break;
-      case ActivityType.SCHEDULED_SURVEY_REMINDER:
-        final String userId = feed.data!["id"] as String;
-        // TODO: Handle this case.
-        break;
-      case ActivityType.NEW_USER_REGISTERED:
-        final String userId = feed.data!["id"] as String;
-        final User aimedUser = await _userRepository.findById(userId);
+  Future<void> requestPermissions() async {
+    final NotificationSettings settings =
+        await _firebaseMessaging.requestPermission(
+      announcement: true,
+      carPlay: true,
+      criticalAlert: true,
+    );
 
-        // TODO: Handle this case.
-        break;
-      case ActivityType.SETTINGS_UPDATED:
-        _settingsController.getSettings();
-        break;
-      case ActivityType.USER_CONFIRMED:
-        _launcherProvider.signOut();
-        break;
+    if (kDebugMode) {
+      late String message;
+      if (settings.authorizationStatus == AuthorizationStatus.authorized) {
+        message = 'User granted permission.';
+      } else if (settings.authorizationStatus ==
+          AuthorizationStatus.provisional) {
+        message = 'User granted provisional permission.';
+      } else {
+        message = 'User declined or has not accepted permission.';
+      }
+      print(Colorize("Notifications: $message").blue());
     }
+  }
+
+  ///
+  void handleMessage(RemoteMessage message) {
+    if (kDebugMode) {
+      print(
+        Colorize('Notifications:\nHandling a Message $message').blue(),
+      );
+    }
+    final ActivityFeed feed = ActivityFeed.fromJson(message.data);
+    feeds.add(feed.copyWith(id: Uuid().v4()));
+    state = ActivityFeedState.data(notifications: feeds);
   }
 
   ///
@@ -112,15 +172,25 @@ class ActivityFeedNotifier extends StateNotifier<ActivityFeedState> {
 
   ///
   void init() {
-    _subscription = _feedProvider.listen(
-      (RemoteMessage message) {
-        final ActivityFeed feed = ActivityFeed.fromJson(message.data);
-        feeds.add(feed);
-        state = ActivityFeedState.data(notifications: feeds);
+    _firebaseMessaging.setForegroundNotificationPresentationOptions(
+      alert: true,
+      badge: true,
+      sound: true,
+    );
+
+    _firebaseMessaging.getInitialMessage().then(
+      (RemoteMessage? message) {
+        if (message != null) {
+          handleMessage(message);
+        }
       },
+    );
+
+    _subscription = _feedOnMessageProvider.listen(
+      handleMessage,
       onError: (Object error, StackTrace trace) {
         if (kDebugMode) {
-          print(Colorize(error.toString()).red());
+          print(Colorize('Notifications: ${error.toString()}').red());
         }
         state = ActivityFeedState.error(
           ActivityFeedFailure(reason: ActivityFeedFailureReason.unknown),
@@ -129,35 +199,4 @@ class ActivityFeedNotifier extends StateNotifier<ActivityFeedState> {
       onDone: () => state = const ActivityFeedState.completed(),
     );
   }
-
-  /*
-
-  void init() {
-    _subscriptionOnCreateSurveyOperation = apiCategory.subscribe(
-        request:
-            GraphQLRequest<String>(document: graphQLDocumentOnCreateSurvey),
-        onData: (event) {
-          final Map<String, dynamic> data =
-              json.decode(event.data as String) as Map<String, dynamic>;
-          final Map<String, dynamic> result =
-              data['onCreateSurvey'] as Map<String, dynamic>;
-          final ActivityFeed activityFeed = ActivityFeed(
-            description:
-                'A new survey ${result['title']} is available for you.',
-            id: uuid.v4(),
-            type: ActivityType.SCHEDULED_SURVEY_REMINDER,
-            issuerID: result['id'] as String,
-          );
-          _notifications.add(activityFeed);
-        },
-        onEstablished: () {
-          print('Subscription established');
-        },
-        onError: (error) {
-          print('Subscription failed with error: $error');
-        },
-        onDone: () {
-          print('Subscription has been closed successfully');
-        });
-  }*/
 }
